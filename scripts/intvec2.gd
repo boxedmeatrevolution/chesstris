e54c29e1c2bdfea1a991327d320dc8a369275777
Duane Byer <duane.byer@gmail.com>
2021-04-24 12:27:39 -0400

    Add intvec type

---
+++ scripts/intvec2.gd
@@ -0,0 +1,19 @@
+extends Object
+
+class_name IntVec2
+
+var x : int = 0
+var y : int = 0
+
+func _init(x_ : int, y_ : int) -> void:
+	self.x = x_
+	self.y = y_
+
+func add(other : IntVec2) -> IntVec2:
+	return get_script().new(other.x + self.x, other.y + self.y)
+
+func sub(other : IntVec2) -> IntVec2:
+	return get_script().new(self.x - other.x, self.y - other.y)
+
+func scale(other : int) -> IntVec2:
+	return get_script().new(other * self.x, other * self.y)
