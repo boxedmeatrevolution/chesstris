aaa5aba8343523505cd05d9d08da7c5208ae3714
asemahle <aidan.mahler13@gmail.com>
2021-04-24 14:56:08 -0400

    phases

--- scripts/logic_manager.gd
+++ scripts/logic_manager.gd
@@ -1,15 +1,17 @@
 extends Node
 
 const WIDTH : int = 6
-const HEIGHT : int = 6
+const HEIGHT : int = 9
+const SPAWN_ROWS : int = 3 # top 3 rows are for spawning pieces
 
 var next_object_id : int = 1
 
+var phase : int = Phases.PLAYER_MOVE
 var level : int = 1
 var moves = {
-	'first': MoveType.PAWN,
-	'second': MoveType.PAWN,
-	'third': MoveType.PAWN
+	'first': MoveType.GOOD_PAWN,
+	'second': MoveType.GOOD_PAWN,
+	'third': MoveType.GOOD_PAWN
 }
 var next_move = MoveType.PAWN
 var enemy_ids = [] # list of ids
@@ -18,7 +20,7 @@ var player = PieceLogic.new({
 	'id': player_id,
 	'is_player': true,
 	'pos': IntVec2.new(1,1),
-	'type': MoveType.PAWN
+	'type': MoveType.GOOD_PAWN
 })
 var pieces = {  # keys are piece ids
 	player_id: player
@@ -31,6 +33,7 @@ signal move_enemy(id, new_pos) # int and IntVec2
 signal enemy_death(id, old_pos) # int and IntVec2
 signal pawn_promotion(id, pos) # int and IntVec2
 signal move_draw(type, slot) # MoveType and int
+signal phase_change(new_phase) # Phases
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -57,6 +60,33 @@ func increment_phase():
 # Increments the phase if the move is legal
 func try_player_move(slot: int, pos: IntVec2) -> bool:
 	return false
+
+# Type is a MoveType
+# Returns: 
+#   array of IntVec2
+func get_legal_moves(pos: IntVec2, type: int) -> Array:
+	var moves = []
+	if (type == MoveType.BAD_PAWN):
+		var move = IntVec2.new(pos.x, pos.y - 1)
+		if (is_on_board_and_empty(move)):
+			moves.push_back(IntVec2.new(pos.x, pos.y - 1))
+	elif (type == MoveType.GOOD_PAWN):
+		var move = IntVec2.new(pos.x, pos.y + 1)
+		if (is_on_play_area_and_empty(move)):
+			moves.push_back(IntVec2.new(pos.x, pos.y + 1))
+	return moves
+
+func is_on_board_and_empty(pos: IntVec2) -> bool:
+	return is_on_board(pos) && board[pos.x][pos.y] == null
+
+func is_on_board(pos: IntVec2) -> bool:
+	return pos.x >= 0 && pos.y >= 0 && pos.x < WIDTH && pos.y < HEIGHT
+
+func is_on_play_area_and_empty(pos: IntVec2) -> bool: 
+	return is_on_play_area(pos) && board[pos.x][pos.y] == null
+
+func is_on_play_area(pos: IntVec2) -> bool: 
+	return pos.x >= 0 && pos.y >= 0 && pos.x < WIDTH && pos.y < HEIGHT - SPAWN_ROWS
 
 # Adds a piece to the board at the given location.
 # Requires: the pos is not occupied
--- scripts/move_type.gd
+++ scripts/move_type.gd
@@ -2,9 +2,10 @@ extends Reference
 class_name MoveType
 
 const NULL = 0
-const PAWN = 1
+const BAD_PAWN = 1
 const KNIGHT = 2
 const BISHOP = 3
 const ROOK = 4
 const QUEEN = 5 
 const KING = 6
+const GOOD_PAWN = 7
