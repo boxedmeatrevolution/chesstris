422624cd8a7ba7c98a227581b3689b7b81bbd35f
asemahle <aidan.mahler13@gmail.com>
2021-04-25 12:12:03 -0400

    add method for converting phase to string

--- scripts/phases.gd
+++ scripts/phases.gd
@@ -7,3 +7,14 @@ const PAWN_MOVE = 2
 const SPAWN_ENEMY = 3
 const PRE_GAME = 4
 const GAME_OVER = 5
+const YOU_WIN = 6
+
+static func string(phase : int) -> String:
+	var phases = [
+		"PLAYER_MOVE", "QUEEN_MOVE", "PAWN_MOVE", "SPAWN_ENEMY",
+		"PRE_GAME", "GAME_OVER", "YOU_WIN"
+	]
+	if phase < 0 || phase >= phases.size():
+		return "Invalid phase value %s" % phase
+	return phases[phase]
+	
